4257748278cc976b9a0698e73eb92bbbb6e9b539
Tom Genoni <tom@genoni.studio>
2024-11-13 14:31:22 -0800

    Change swift to ios

--- src/ios/buildSymbols.sh
+++ src/ios/buildSymbols.sh
@@ -2,7 +2,7 @@
 
 # Set source and destination directories
 INPUT_DIR="./svgs"
-OUTPUT_DIR="./dist/swift"
+OUTPUT_DIR="./dist/ios"
 
 # Loop through all SVG files in the source directory
 # Loop through all SVG files in the directory
@@ -14,12 +14,10 @@ for file in "$INPUT_DIR"/*.svg; do
     filename_no_ext="${filename%.*}"
 
     output_sfsymbol="$OUTPUT_DIR/${filename_no_ext}-symbol.svg"
-    output_swift="$OUTPUT_DIR/${filename_no_ext}.swift"
 
     # Run the swiftdraw command on each file
     echo "Processing $file..."
-    swiftdraw "$file" --format sfsymbol --insets auto --output "$output_sfsymbol"
-    swiftdraw "$file" --format swift --insets auto --output "$output_swift"
+    swiftdraw "$file" --format sfsymbol --insets 40,30,40,30 --output "$output_sfsymbol"
 
   else
     echo "No SVG files found in $INPUT_DIR"
